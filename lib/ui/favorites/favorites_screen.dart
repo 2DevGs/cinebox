af997794799f4815fe932e62ec1a53a728523927
2DevGs <dev.gustavoserrano@gmail.com>
2025-09-11 17:22:17 -0300

    Favorites Screen

--- lib/ui/favorites/favorites_screen.dart
+++ lib/ui/favorites/favorites_screen.dart
@@ -1,14 +1,93 @@
+import 'package:cinebox/ui/core/widgets/loader_messages.dart';
+import 'package:cinebox/ui/core/widgets/movie_card.dart';
+import 'package:cinebox/ui/favorites/commands/get_favorites_command.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class FavoritesScreen extends StatelessWidget {
+import '../../domain/models/favorite_movie.dart';
+import 'favorites_view_model.dart';
 
-  const FavoritesScreen({ super.key });
+class FavoritesScreen extends ConsumerStatefulWidget {
+  const FavoritesScreen({super.key});
 
-   @override
-   Widget build(BuildContext context) {
-       return Scaffold(
-           appBar: AppBar(title: const Text('Meus Favoritos'),),
-           body: Container(),
-       );
+  @override
+  ConsumerState<FavoritesScreen> createState() => _FavoritesScreenState();
+}
+
+class _FavoritesScreenState extends ConsumerState<FavoritesScreen> with LoaderAndMessage {
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      ref.read(favoritesViewModelProvider).fetchFavorites();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final favoritesMovies = ref.watch(getFavoritesCommandProvider);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Meus Favoritos'),
+      ),
+      body: favoritesMovies.when(
+        loading: () => Center(
+          child: CircularProgressIndicator(),
+        ),
+        error: (error, stackTrace) => Center(
+          child: Text('Erro ao buscar títulos favoritos'),
+        ),
+        data: (data) {
+          if (data.isEmpty) {
+            return Center(
+              child: Text('Nenhum título cadastrado como favorito.'),
+            );
+          }
+          return CustomScrollView(
+            slivers: [
+              SliverPadding(
+                padding: EdgeInsetsGeometry.symmetric(horizontal: 16),
+                sliver: SliverGrid(
+                  gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+                    maxCrossAxisExtent: 148,
+                    mainAxisExtent: 268,
+                  ),
+                  delegate: SliverChildBuilderDelegate(
+                    (context, index) {
+                      final FavoriteMovie(
+                        :id,
+                        :title,
+                        :year,
+                        posterPath: imageUrl,
+                      ) = data[index];
+                      return Container(
+                        margin: EdgeInsets.all(8),
+                        child: MovieCard(
+                          key: UniqueKey(),
+                          id: id,
+                          title: title,
+                          year: year,
+                          imageUrl: imageUrl,
+                          isFavorite: true,
+                          onFavoriteTap: () async {
+                            showLoader();
+                            await ref
+                                .read(favoritesViewModelProvider)
+                                .deleteFavoriteMovie(id);
+                            hideLoader();
+                          },
+                        ),
+                      );
+                    },
+                    childCount: data.length,
+                  ),
+                ),
+              ),
+            ],
+          );
+        },
+      ),
+    );
   }
 }
